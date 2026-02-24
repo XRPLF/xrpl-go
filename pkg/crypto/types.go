c36136af9888356f8d85ef893d4777266400d36f
Kevin Pita <kpita@peersyst.com>
2026-02-24 13:25:10 +0100

    refactor(address-codec): set ed25519 family seed prefix

--- pkg/crypto/types.go
+++ pkg/crypto/types.go
@@ -3,5 +3,5 @@ package crypto
 // Algorithm defines the interface for cryptographic algorithms used in XRPL key generation and signing.
 type Algorithm interface {
 	Prefix() byte
-	FamilySeedPrefix() byte
+	FamilySeedPrefix() []byte
 }
