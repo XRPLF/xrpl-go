4c77d36b14be9a13fff16b42526e1d8971efa25b
aluque-peersyst <aluque@peersyst.com>
2026-03-19 15:16:30 +0100

    test(account): add integration test for AccountSet

---
+++ xrpl/transaction/integration/account_set_test.go
@@ -0,0 +1,97 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/rpc"
+	"github.com/Peersyst/xrpl-go/xrpl/testutil/integration"
+	"github.com/Peersyst/xrpl-go/xrpl/transaction"
+	"github.com/Peersyst/xrpl-go/xrpl/websocket"
+	"github.com/stretchr/testify/require"
+)
+
+type AccountSet struct {
+	Name          string
+	AccountSet    *transaction.AccountSet
+	ExpectedError string
+}
+
+func TestIntegrationAccountSet_Websocket(t *testing.T) {
+	env := integration.GetWebsocketEnv(t)
+	client := websocket.NewClient(websocket.NewClientConfig().WithHost(env.Host).WithFaucetProvider(env.FaucetProvider))
+
+	runner := integration.NewRunner(t, client, &integration.RunnerConfig{
+		WalletCount: 1,
+	})
+
+	err := runner.Setup()
+	require.NoError(t, err)
+	defer runner.Teardown()
+
+	sender := runner.GetWallet(0)
+
+	tt := []AccountSet{
+		{
+			Name: "delete account",
+			AccountSet: &transaction.AccountSet{
+				BaseTx: transaction.BaseTx{
+					Account: sender.GetAddress(),
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			flatTx := tc.AccountSet.Flatten()
+			_, err := runner.TestTransaction(&flatTx, sender, "tesSUCCESS", nil)
+			if tc.ExpectedError != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.ExpectedError)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestIntegrationAccountSet_RPCClient(t *testing.T) {
+	env := integration.GetRPCEnv(t)
+	clientCfg, err := rpc.NewClientConfig(env.Host, rpc.WithFaucetProvider(env.FaucetProvider))
+	require.NoError(t, err)
+	client := rpc.NewClient(clientCfg)
+
+	runner := integration.NewRunner(t, client, integration.NewRunnerConfig(
+		integration.WithWallets(1),
+	))
+
+	err = runner.Setup()
+	require.NoError(t, err)
+	defer runner.Teardown()
+
+	sender := runner.GetWallet(0)
+
+	tt := []AccountSet{
+		{
+			Name: "delete account",
+			AccountSet: &transaction.AccountSet{
+				BaseTx: transaction.BaseTx{
+					Account: sender.GetAddress(),
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			flatTx := tc.AccountSet.Flatten()
+			_, err := runner.TestTransaction(&flatTx, sender, "tesSUCCESS", nil)
+			if tc.ExpectedError != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.ExpectedError)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
