8507768e69f575922d2738b09806b7377e37c950
aluque-peersyst <aluque@peersyst.com>
2026-03-26 10:09:22 +0100

    refactor(account): extracted account test function

--- xrpl/transaction/integration/account_set_test.go
+++ xrpl/transaction/integration/account_set_test.go
@@ -10,16 +10,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type AccountSet struct {
+type AccountSetTest struct {
 	Name          string
 	AccountSet    *transaction.AccountSet
 	ExpectedError string
 }
 
-func TestIntegrationAccountSet_Websocket(t *testing.T) {
-	env := integration.GetWebsocketEnv(t)
-	client := websocket.NewClient(websocket.NewClientConfig().WithHost(env.Host).WithFaucetProvider(env.FaucetProvider))
-
+func testIntegrationAccountSet(t *testing.T, client integration.Client) {
 	runner := integration.NewRunner(t, client, &integration.RunnerConfig{
 		WalletCount: 1,
 	})
@@ -30,9 +27,9 @@ func TestIntegrationAccountSet_Websocket(t *testing.T) {
 
 	sender := runner.GetWallet(0)
 
-	tt := []AccountSet{
+	tt := []AccountSetTest{
 		{
-			Name: "set account",
+			Name: "pass - set account",
 			AccountSet: &transaction.AccountSet{
 				BaseTx: transaction.BaseTx{
 					Account: sender.GetAddress(),
@@ -55,43 +52,16 @@ func TestIntegrationAccountSet_Websocket(t *testing.T) {
 	}
 }
 
+func TestIntegrationAccountSet_Websocket(t *testing.T) {
+	env := integration.GetWebsocketEnv(t)
+	client := websocket.NewClient(websocket.NewClientConfig().WithHost(env.Host).WithFaucetProvider(env.FaucetProvider))
+	testIntegrationAccountSet(t, client)
+}
+
 func TestIntegrationAccountSet_RPCClient(t *testing.T) {
 	env := integration.GetRPCEnv(t)
 	clientCfg, err := rpc.NewClientConfig(env.Host, rpc.WithFaucetProvider(env.FaucetProvider))
 	require.NoError(t, err)
 	client := rpc.NewClient(clientCfg)
-
-	runner := integration.NewRunner(t, client, integration.NewRunnerConfig(
-		integration.WithWallets(1),
-	))
-
-	err = runner.Setup()
-	require.NoError(t, err)
-	defer runner.Teardown()
-
-	sender := runner.GetWallet(0)
-
-	tt := []AccountSet{
-		{
-			Name: "set account",
-			AccountSet: &transaction.AccountSet{
-				BaseTx: transaction.BaseTx{
-					Account: sender.GetAddress(),
-				},
-			},
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.Name, func(t *testing.T) {
-			flatTx := tc.AccountSet.Flatten()
-			_, err := runner.TestTransaction(&flatTx, sender, "tesSUCCESS", nil)
-			if tc.ExpectedError != "" {
-				require.Error(t, err)
-				require.Contains(t, err.Error(), tc.ExpectedError)
-			} else {
-				require.NoError(t, err)
-			}
-		})
-	}
+	testIntegrationAccountSet(t, client)
 }
