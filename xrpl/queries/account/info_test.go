83b04d65586f8c2db4fca9fa51ae968c104c031a
Kevin Pita <kpita@peersyst.com>
2026-09-15 18:03:49 +0200

    feat(ledger): expose pseudo-account links

--- xrpl/queries/account/info_test.go
+++ xrpl/queries/account/info_test.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/Peersyst/xrpl-go/xrpl/ledger-entry-types"
@@ -8,6 +9,7 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/queries/common"
 	"github.com/Peersyst/xrpl-go/xrpl/testutil"
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccountInfoRequest(t *testing.T) {
@@ -28,6 +30,41 @@ func TestAccountInfoRequest(t *testing.T) {
 }`
 	if err := testutil.Serialize(t, s, j); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestAccountInfoResponsePseudoAccountLinks(t *testing.T) {
+	const id = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF"
+	tests := []struct {
+		name                         string
+		ammID, vaultID, loanBrokerID types.Hash256
+		linkJSON                     string
+	}{
+		{name: "ordinary account"},
+		{name: "AMM account", ammID: id, linkJSON: `,"AMMID":"` + id + `"`},
+		{name: "vault account", vaultID: id, linkJSON: `,"VaultID":"` + id + `"`},
+		{name: "loan broker account", loanBrokerID: id, linkJSON: `,"LoanBrokerID":"` + id + `"`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := InfoResponse{
+				AccountData: ledger.AccountRoot{
+					Account:         "rLUEXYuLiQptky37CqLcm9USQpPiz5rkpD",
+					LedgerEntryType: ledger.AccountRootEntry,
+					AMMID:           tt.ammID,
+					VaultID:         tt.vaultID,
+					LoanBrokerID:    tt.loanBrokerID,
+				},
+				Validated: true,
+			}
+			j := `{"account_data":{"Account":"rLUEXYuLiQptky37CqLcm9USQpPiz5rkpD","LedgerEntryType":"AccountRoot","Flags":0,"OwnerCount":0,"PreviousTxnID":"","PreviousTxnLgrSeq":0,"Sequence":0` + tt.linkJSON + `},"validated":true}`
+			encoded, err := json.Marshal(s)
+			require.NoError(t, err)
+			require.JSONEq(t, j, string(encoded))
+			var decoded InfoResponse
+			require.NoError(t, json.Unmarshal([]byte(j), &decoded))
+			require.Equal(t, s, decoded)
+		})
 	}
 }
 
